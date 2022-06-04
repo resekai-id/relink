f63d2cf3bf3b146310e611978d9f30baed47e988
extraarya <aryaalikhani@icloud.com>
2022-06-04 16:42:21 -0400

    feat: HeaderLink component

---
+++ src/components/HeaderLink.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const HeaderLink = styled.a`
+  font-family: var(--font-primary);
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+
+  text-decoration: none;
+
+  color: var(--color-text-primary);
+`;
+
+export default HeaderLink;
