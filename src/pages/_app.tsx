696db6359d94d6057d54f81c5bb24a8f46969fc8
extraarya <aryaalikhani@icloud.com>
2022-06-04 16:40:03 -0400

    feat: implement PageContainer in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import 'normalize.css';
-import '../../public/assets/styles/fonts.css';
+
+import '../assets/styles/fonts.css';
 
 import {Fragment} from 'react';
 import {NextComponentType, NextPageContext} from 'next';
@@ -7,13 +8,21 @@ import NextApp, {AppContext} from 'next/app';
 
 import store from '../store';
 
-import ThemeProvider from '../containers/ThemeProvider';
-import Header from '../containers/Header';
-import PageWrapper from '../containers/PageWrapper';
+import ThemeProvider from '../components/ThemeProvider';
+import PageHeader from '../containers/PageHeader';
+import PageContainer from '../components/PageContainer';
 
 class App extends NextApp {
   public static async getInitialProps({Component, ctx}: AppContext) {
     return {pageProps: Component.getInitialProps ? await Component.getInitialProps(ctx) : {}};
+  }
+
+  public static getStaticProps() {
+    return {
+      props: {},
+      // Unsplash API limit is 50 requests per hour.
+      revalidate: 72,
+    };
   }
 
   public render() {
@@ -24,11 +33,11 @@ class App extends NextApp {
 
     return (
       <Fragment>
-        <PageWrapper>
-          <ThemeProvider />
-          <Header />
+        <ThemeProvider />
+        <PageHeader />
+        <PageContainer>
           <Component {...pageProps} />
-        </PageWrapper>
+        </PageContainer>
       </Fragment>
     );
   }
