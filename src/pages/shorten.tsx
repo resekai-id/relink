f23d6eb0f0efbcf614dcdeca18df32b713459b6a
extraarya <aryaalikhani@icloud.com>
2022-06-12 17:26:16 -0400

    refactor: remove unused props on ShortenPage

--- src/pages/shorten.tsx
+++ src/pages/shorten.tsx
@@ -3,17 +3,11 @@ import {Fragment} from 'react';
 
 // import ShortenForm from '../containers/ShortenForm';
 
-export interface ShortenPageProps {
-  hCaptchaSiteKey: string;
-}
-
-const ShortenPage: NextPage<ShortenPageProps> = ({hCaptchaSiteKey}) => (
+const ShortenPage: NextPage = () => (
   <Fragment>
     {/* <ShortenForm hCaptchaSiteKey={hCaptchaSiteKey} /> */}
     <p>Hello World!</p>
   </Fragment>
 );
-
-export const getStaticProps = () => ({props: {hCaptchaSiteKey: process.env.HCAPTCHA_SITE_KEY}});
 
 export default ShortenPage;
