d2830b93b89be69342a8bfee11912174f43ed35a
extraarya <aryaalikhani@icloud.com>
2022-06-15 15:16:49 -0400

    feat: IconButton

---
+++ src/components/IconButton.tsx
@@ -0,0 +1,37 @@
+import {ButtonHTMLAttributes, FC, SVGAttributes} from 'react';
+import styled from 'styled-components';
+
+export interface IconButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  icon: FC<SVGAttributes<SVGElement>>;
+}
+
+const Container = styled.button`
+  cursor: pointer;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+
+  padding: 0.875em;
+
+  background-color: var(--color-text-link);
+
+  border: none;
+  border-radius: 0.25em;
+
+  :hover {
+    box-shadow: var(--shadow-primary);
+  }
+
+  svg {
+    width: 1em;
+    height: auto;
+
+    color: var(--color-background-primary);
+  }
+`;
+
+const IconButton: FC<IconButtonProps> = props => <Container {...props}>{<props.icon />}</Container>;
+
+export default IconButton;
